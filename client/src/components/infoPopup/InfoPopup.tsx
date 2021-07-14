735b9ab9eb9352ea0b5406261f658ef030cf8797
Dominik Rumian <xrumian@fi.muni.cz>
2021-07-14 22:25:14 +0200

    fade in of info popup added

--- client/src/components/infoPopup/InfoPopup.tsx
+++ client/src/components/infoPopup/InfoPopup.tsx
@@ -1,10 +1,20 @@
 import React from 'react';
 import Popup from 'reactjs-popup';
 import {Button} from "react-bootstrap";
+import Card from "react-bootstrap/Card";
 import 'reactjs-popup/dist/index.css';
+import './InfoPopup.css';
 
 export const InfoPopup = () => (
-    <Popup trigger={<Button> Info </Button>} modal nested>
-        <span> Modal content </span>
+    <Popup
+        trigger={
+            <Button type="button" className="button">
+                Info
+            </Button>
+        }
+        position={'left top'}
+        on={['hover', 'focus']}
+    >
+        <Card title={'left top'}>Click as fast as you can for 10 seconds!</Card>
     </Popup>
 );
